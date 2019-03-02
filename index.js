4b5df9b8ea9e475c0a013669d50373d4cf0e137d
Heinhtet Naing <heinhtetnaing186@gmail.com>
2019-03-02 16:29:25 +0630

    STUCK

--- index.js
+++ index.js
@@ -1,90 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-function Post(){
-
+function List() {
 	return(
-		<div className="post">
+		<div className="list">
+		<div>To Do List</div>
 		
-			<Profile/>
+		<div id="todolist">hihihi</div>
+	
 
-			<div className = "content">
-				<Text/>
-			</div>
-			
-
-			<div>
-				<Likes/>
-				<Comments/>
-				<Share/>
-			</div>
-			
-		</div>	
-	);
-}
-
-function Profile() {
-
-	return(
-		<div className="profile">
-		<img
-			src="https://www.gravatar.com/avatar/nothing"
-			className="pp"
-			alt="pp"
-		/>
-
-		<div>
-			<Name/>
-			<Timestamp/>
+		<div className="inputUI">
+		<Input/>
+		<Enter/>
 		</div>
 
 		</div>
 	);
 }
 
-function Text(){
+function Input(){
 	return(
-		<div className="text">
-			Text
-		</div>
-
+		<span className="inputbox"><input id="input" type="text" /></span>
 	);
 }
 
-function Name(){
+const Enter =()=>{
 	return(
-		<span className="name">
-			Profile Name
-		</span>
-
+		<span className="enterbutton"><button onClick={add}>Enter</button></span>
 	);
 }
 
-const Likes = () =>{
+function add(){
+	var saved = document.getElementById("input").value;
+	
+	<item todo = {saved} />
+
+}
+
+const item =(todo)=> {
 	return(
-		<button><i class="fa fa-thumbs-o-up"/></button>
+		<div>todo</div>
 	);
 }
 
-const Comments = () => {
-	return(
-		<button><i class="fa fa-comment-o"></i></button>
-	);
-}
 
-const Share = () => {
-	return(
-		<button><i class="fa fa-share"></i></button>
-	);
-}
-
-function Timestamp() {
-	return(
-		<div className="timestamp">
-			time
-		</div>
-	);
-}
-
-ReactDOM.render( <Post/> , document.querySelector("#root"));
+ReactDOM.render(<List/> , document.querySelector("#root"));
